671210c31aedabb56d5187668ab207a3bc23655e
Aidan <lizardswimmer@gmail.com>
2019-10-03 23:16:35 -0700

    Added tests for HashingArray

--- on_image/HashingArray.py
+++ on_image/HashingArray.py
@@ -11,20 +11,16 @@ class HashingArray(np.ndarray):
     def __hash__(self):
         h = xxhash.xxh64()
         h.update(self)
+        if self.filename:
+            h.update(self.filename)
         return h.intdigest()
 
     def __eq__(self, other):
-        return hash(self) == hash(other)
+        if hasattr(other, "filename") and self.filename != other.filename:
+            return False
+        return super().__eq__(other)
 
     def __repr__(self):  # Just makes it look nicer in dicts
         if self.filename:
             return f"HashingArray({self.filename})"
         return f"HashingArray({hash(self)})"
-
-
-if __name__ == "__main__":
-    from PIL import Image
-    img_pillow = Image.new("RGB", (1920, 1080), (255, 255, 255))
-    img_cv2 = HashingArray(img_pillow, filename="test.png")
-    print(hash(img_cv2))
-    print(repr(img_cv2))
---
+++ on_image/tests/test_HashingArray.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+import numpy as np
+from on_image.HashingArray import HashingArray
+
+class TestHashingArray(TestCase):
+    def setUp(self) -> None:
+        self.sample_np_array = np.zeros((100, 100))
+        self.sample_hashing_array = HashingArray(self.sample_np_array)
+
+    def test_hash(self):
+        self.assertEqual(hash(self.sample_hashing_array), 1608203990122525651)
+
+    def test_eq(self):
+        self.assertTrue(np.all(self.sample_hashing_array == self.sample_np_array))
+        self.assertFalse(np.all(self.sample_hashing_array == np.ones((100, 100))))
+        self.assertFalse(np.all(self.sample_hashing_array == HashingArray(self.sample_np_array, filename="something")))
+
+    def test_numpy_function(self):
+        new_arr = np.reshape(self.sample_hashing_array, (1, 100 * 100))
+        self.assertTrue(np.all(new_arr == np.zeros((1, 100*100))))
